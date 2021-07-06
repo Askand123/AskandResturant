31bfddb77ca13295b56b8bb5ca46337208c1b422
Askand123 <84070907+Askand123@users.noreply.github.com>
2021-07-06 14:19:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+var form = document.getElementById('sheetdb-form');
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        fetch(form.action, {
+            method : "POST",
+            body: new FormData(document.getElementById("sheetdb-form")),
+        }).then(
+            response => response.json()
+        ).then((html) => {
+        // you can put any JS code here
+        alert('Your Order is Accepted by A.S. Devleopers - Thankyou')
+    });
+});
